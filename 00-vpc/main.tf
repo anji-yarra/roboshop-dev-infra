e2d5c9da356783a0ed576120b4e99e80a302b3ed
tidingdbruh <tidingdbruh@gmail.com>
2026-06-30 12:27:09 +0530

    fgfthh

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/anji-yarra/terraform-aws-vpc.git//terraform-aws-vpc?ref=main"
+    source = "git::https://github.com/anji-yarra/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = false
